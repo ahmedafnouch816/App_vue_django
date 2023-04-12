f834b2fbb2d4fd6ac469ff022cf6b608e18f708e
ahmedafnouch <ahmedafnouch17@gmail.com>
2023-04-12 13:30:53 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+ <h1 class="tit">hello Ahmed </h1>
+ <h2>Welecome to my website </h2>
+
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #203F5F;
+  margin-top: 60px;
+}
+.tit{
+  color: red;
+}
+
+h2{
+  color: green;
+}
+</style>
